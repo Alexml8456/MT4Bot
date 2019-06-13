127a17b5c3b829973849e2087e957a8d8d125210
amelnychuk <alexander.melnychuk@kstt.ua>
2019-06-13 13:15:58 +0300

    Added several new conditions

--- src/main/java/com/alex/Main.java
+++ src/main/java/com/alex/Main.java
@@ -1,15 +1,19 @@
 package com.alex;
 
-import com.opencsv.CSVReader;
-import com.opencsv.CSVWriter;
-
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        System.out.println(conditionSecondToSell());
+        System.out.println(conditionSecondToBuy());
+    }
 
+
+    private static boolean conditionSecondToSell() {
+        double test = 0.09;
+        return test <= 0.0999 && test >= -0.0999;
+    }
+
+    private static boolean conditionSecondToBuy() {
+        double test = 0.0933;
+        return test <= 0.0999 && test >= -0.0999;
     }
 }
--- src/main/java/com/alex/services/TradeCondition.java
+++ src/main/java/com/alex/services/TradeCondition.java
@@ -42,11 +42,11 @@ public class TradeCondition {
                 lastConditionTime = DateTime.getGMTTimeMillis();
                 log.info(getValues("Sell-based on TFX values"));
                 telegramBot.pushMessage(dataHolder.getSubscriptions(), getValues("Sell-based on TFX values"));
-            } else if (conditionOneToBuy()) {
+            } else if (conditionThirdToBuy()) {
                 lastConditionTime = DateTime.getGMTTimeMillis();
                 log.info(getValues("Buy-based on SS values"));
                 telegramBot.pushMessage(dataHolder.getSubscriptions(), getValues("Buy-based on SS values"));
-            } else if (conditionOneToSell()) {
+            } else if (conditionThirdToSell()) {
                 lastConditionTime = DateTime.getGMTTimeMillis();
                 log.info(getValues("Sell-based on SS values"));
                 telegramBot.pushMessage(dataHolder.getSubscriptions(), getValues("Sell-based on SS values"));
@@ -76,12 +76,17 @@ public class TradeCondition {
 
     private boolean conditionThirdToBuy() {
         int mapSize = timeMetrics.getCsvMetrics().get(15).size();
-        return timeMetrics.getCsvMetrics().get(15).get(mapSize - 1).getSsValue().toString().contains("-0.0");
+        return timeMetrics.getCsvMetrics().get(15).get(mapSize - 1).getSsValue() < -0.1 && conditionNearZero(mapSize);
     }
 
     private boolean conditionThirdToSell() {
         int mapSize = timeMetrics.getCsvMetrics().get(15).size();
-        return timeMetrics.getCsvMetrics().get(15).get(mapSize - 1).getSsValue().toString().contains("0.0");
+        return timeMetrics.getCsvMetrics().get(15).get(mapSize - 1).getSsValue() > 0.1 && conditionNearZero(mapSize);
+    }
+
+    private boolean conditionNearZero(Integer mapSize) {
+        double lastValue = timeMetrics.getCsvMetrics().get(15).get(mapSize - 1).getSsValue();
+        return lastValue <= 0.0999 && lastValue >= -0.0999;
     }
 
     private String getValues(String direction) {
