42eed1e8372a937d09263d25803ca8e25e0c2d7c
linchalex@gmail.com <eximb_com2007>
2019-07-21 21:06:14 +0300

    Added filter to csv files

--- src/main/java/com/alex/services/FileOperations.java
+++ src/main/java/com/alex/services/FileOperations.java
@@ -29,7 +29,7 @@ public class FileOperations {
     public void saveFilesToList() {
         try (Stream<Path> walk = Files.walk(Paths.get(mt4Folder))) {
 
-            List<String> result = walk.filter(Files::isRegularFile)
+            List<String> result = walk.filter(f -> f.endsWith(".csv"))
                     .map(Path::toString).collect(Collectors.toList());
 
             if (result.size() > 0) {
