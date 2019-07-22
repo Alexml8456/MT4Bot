cfd30f0888086aac4e27029ee57f3aa1334b5bcd
linchalex@gmail.com <eximb_com2007>
2019-07-23 00:11:45 +0300

    added save gif files function

--- src/main/java/com/alex/Main.java
+++ src/main/java/com/alex/Main.java
@@ -4,9 +4,20 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        saveFilesToList();
+
+    }
+
+    private static void margeFiles() throws IOException {
         int rows = 2;   //we assume the no. of rows and cols are known and each chunk has equal width and height
         int cols = 2;
         int chunks = rows * cols;
@@ -40,6 +51,18 @@ public class Main {
         }
         System.out.println("Image concatenated.....");
         ImageIO.write(finalImg, "jpeg", new File("finalImg.gif"));
+    }
+
+    public static void saveFilesToList() {
+        try (Stream<Path> walk = Files.walk(Paths.get("/home/alexml/.wine/drive_c/Program Files (x86)/ForexClub MT4/MQL4/Files"))) {
+
+            List<String> result = walk.filter(f -> f.getFileName().toString().endsWith(".gif"))
+                    .map(Path::toString).collect(Collectors.toList());
+            System.out.println(result);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        //log.info("List of files has been saved!");
     }
 
 }
