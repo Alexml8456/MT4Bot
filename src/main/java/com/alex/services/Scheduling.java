6affa96334e48c2358f8dbedebccb466324b8ae6
Alexml <alexander.melnychuk@kstt.ua>
2019-10-31 11:56:58 +0200

    change settings for prod

--- src/main/java/com/alex/services/Scheduling.java
+++ src/main/java/com/alex/services/Scheduling.java
@@ -79,7 +79,7 @@ public class Scheduling {
     }
 
 
-    @Scheduled(cron = "05 0/1 * ? * *")
+    @Scheduled(cron = "05 0/5 * ? * *")
     //@Scheduled(cron = "10 0/1 * ? * *")
     public void saveFiles() {
         fileOperations.saveFilesToList();
