693cd15682bafbd38627e140659a525a904301fe
Alexml <Alexander.Melnychuk@kstt.ua>
2022-07-15 20:40:33 +0300

    try to implement buybit push orders

--- src/main/java/com/alex/services/Scheduling.java
+++ src/main/java/com/alex/services/Scheduling.java
@@ -2,18 +2,28 @@ package com.alex.services;
 
 import com.alex.telegram.TelegramBot;
 import com.alex.utils.DateTime;
+import com.alex.utils.HashUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.client.WebSocketConnectionManager;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import javax.websocket.DeploymentException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Optional;
+import java.util.*;
 
 import static java.util.Optional.ofNullable;
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -27,6 +37,9 @@ public class Scheduling {
 
     @Autowired
     private KrakenProcessingService processingService;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Autowired
     private KrakenConnectionService connectionService;
@@ -60,6 +73,29 @@ public class Scheduling {
         //csvOperations.deleteRowsForFile();
         dataHolder.clearFileList();
     }
+
+    @Scheduled(fixedDelay = 60000)
+    public void test() throws InvalidKeyException, NoSuchAlgorithmException {
+        final String API_KEY = "iXeMbH8J0VdoMFHdAy";
+        final String API_SECRET = "ffS6bcky0TjYlWT8HlkmUWOajEgCHumP7fRw";
+        final String url = "https://api.bybit.com/futures/private/order/list?";
+        final String TIMESTAMP = Long.toString(ZonedDateTime.now().toInstant().toEpochMilli());
+        Map<String, Object> map = new TreeMap<>();
+        map.put("api_key", API_KEY);
+        map.put("timestamp", TIMESTAMP);
+        map.put("symbol", "BTCUSDU22");
+        map.put("recv_window", "5000");
+        //map.put("order_status", "Created,New,Filled,Cancelled");
+        String st1 = "api_key=iXeMbH8J0VdoMFHdAy&recv_window=5000"+"&symbol=BTCUSDU22"+"&timestamp="+TIMESTAMP;
+        String signature = HashUtils.hmacSha256(st1,API_SECRET);
+        map.put("sign", signature);
+        String st = st1+"&sign="+signature;
+        log.info(url+st);
+        ResponseEntity<String> response;
+        response = restTemplate.getForEntity(url+st, String.class);
+        log.info(response.getBody());
+    }
+
 
     //@Scheduled(fixedDelay = 60000)
     public void verifyStalePrice() {
@@ -165,5 +201,35 @@ public class Scheduling {
     private boolean isConnected() {
         WebSocketSession session = sessionStorage.getSession();
         return session != null && (session.isOpen() || sessionStorage.isConnecting());
+    }
+
+    private static String genSign(Map<String, Object> params) throws NoSuchAlgorithmException, InvalidKeyException {
+        final String API_SECRET = "ffS6bcky0TjYlWT8HlkmUWOajEgCHumP7fRw";
+        Set<String> keySet = params.keySet();
+        Iterator<String> iter = keySet.iterator();
+        StringBuilder sb = new StringBuilder();
+        while (iter.hasNext()) {
+            String key = iter.next();
+            sb.append(key)
+                    .append("=")
+                    .append(params.get(key))
+                    .append("&");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(API_SECRET.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        sha256_HMAC.init(secret_key);
+
+        return bytesToHex(sha256_HMAC.doFinal(sb.toString().getBytes(StandardCharsets.UTF_8)));
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
     }
 }
